d65eabbdd6dc9039411ce26fbdd3e30c3f23145f
clayball <cwells73@gmail.com>
2016-10-16 16:27:39 -0400

    minor updates.

--- servers/socket-server.py
+++ servers/socket-server.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python
 
+'''
+The purpose of this is to view what different types of TCP traffic look like.
+
+This is a basic example of a socket library based server. To connect to it
+
+$ telnet x.x.x.x 37337
+
+The connect is opened and closed without sending any data.
+
+'''
+
+
 import socket
 
 s = socket.socket()
@@ -14,5 +26,7 @@ s.listen(1)
 while True:
 	conn, addr = s.accept()
 	print '[*] Connection from ', addr
-	conn.send('[*] data received')
+	conn.send('[*] data received\n')
 	conn.close()
+
+print '[*] goodbye'
